e1dddb2ae39b9b77e28a1d96ee0535d3bc135618
RaygozaO <oscar.raygoza@gmail.com>
2024-08-26 17:21:41 -0600

    primer commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { Component,OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import {CommonModule, JsonPipe} from "@angular/common";
+import {RouterOutlet} from "@angular/router";
+import {response} from "express";
+import {FormsModule} from "@angular/forms";
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  imports: [HttpClientModule, JsonPipe, RouterOutlet, CommonModule, FormsModule],
+})
+export class AppComponent implements OnInit {
+  codigoPostal: string = '';
+  colonias: any[] = [];
+  municipio:string = '';
+  entidad: string = '';
+  //data: any;
+  title: string = 'Angular Cli';
+
+  constructor(private http: HttpClient) { }
+  buscarColonia(){
+    if(this.codigoPostal){
+      this.http.get<any[]>(`http://localhost:3000/data?cp=${this.codigoPostal}`).subscribe(
+        response => {
+          this.colonias = response;
+          if(response.length > 0){
+            this.municipio = response[0].nombremunicipio;
+            this.entidad = response[0].nombreentidad;
+          }else {
+            this.municipio = '';
+            this.entidad = '';
+          }
+        },
+        error => { console.log('Error al obtener los datos',error); }
+      )
+    }
+  }
+
+  ngOnInit(): void {
+  }
+}
+
